bf46a767f03cf3ad89cb7b31344d989d36eb9c5a
Artan <artanpl03@gmail.com>
2022-12-09 15:41:39 -0500

    small change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      name: 'Artan'
+    }
+  }
+
+  render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi {this.state.name}
         </p>
+        <button onClick={()=>{
+          this.setState({name:"hio"})
+        }}></button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +34,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
