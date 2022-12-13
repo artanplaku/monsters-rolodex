543e799d7e8c6dd9f91b8173c8a0dcc1133d1949
Artan <artanpl03@gmail.com>
2022-12-13 14:07:31 -0500

    Cardlist is now a functional component

--- src/components/cardlist/Cardlist.jsx
+++ src/components/cardlist/Cardlist.jsx
@@ -1,26 +1,20 @@
-import { Component } from "react";
 import './Cardlist.css'
 import Card from "../card/Card.jsx";
 
-class CardList extends Component{
-    render() {
-        const { filteredMonsters } = this.props
-        
-       return(
-        
-        <div className="cardList">
-            {filteredMonsters.map((monster) => {
-                
-                
-                return(
-                <Card monster={monster}/>
+const CardList = ({filteredMonsters}) => {
+
+    return(
+
+    <div className="cardList" >
+
+        {filteredMonsters.map((monster) => {
+        return(
+        <Card monster={monster}/>
             )
-            })}
-        </div>
+        })}
 
-       ) 
-    }
+    </div>
 
+    ) 
 }
-
 export default CardList;
